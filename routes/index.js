e35d4280733fc333351867e420a6ad31697c2e24
vuy8huyhoang <vuy8huyhoang@gmail.com>
2024-12-05 17:44:27 +0700

    ss

--- routes/index.js
+++ routes/index.js
@@ -4,22 +4,6 @@ var transporter = require('../utils/mailer');
 router.get('/', function (req, res, next) {
     res.render('index', { title: 'Express' });
 });
-router.get('/gui', (req, res) => {
-    const mailOptions = {
-        from: process.env.EMAIL_USER,
-        to: 'vuy8huyhoang@gmail.com',
-        subject: 'Test Email from NodeMailer',
-        text: 'This is a test email sent from NodeMailer!'
-    };
 
-    transporter.sendMail(mailOptions, (error, info) => {
-        if (error) {
-            console.log('Error: ' + error);
-            return res.status(500).send('Failed to send email');
-        }
-        console.log('Email sent: ' + info.response);
-        res.send('Email sent successfully');
-    });
-});
 
 module.exports = router;
