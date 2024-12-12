0bdc6c4f4416304dd7fe11bffe7d71d23980e52f
vuy8huyhoang <vuy8huyhoang@gmail.com>
2024-12-12 10:56:02 +0700

    ss

--- routes/google.js
+++ routes/google.js
@@ -1,5 +1,12 @@
 var express = require('express');
 var router = express.Router();
+var User = require('../models/user');
+const crypto = require('crypto');
+const jwt = require('jsonwebtoken');
+
+
+
+
 
 router.get('/', function (req, res, next) {
     res.render('index', { title: 'Express' });
